e4b3831e8d6ab568d9c021e20bb20352788403f2
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-08-01 23:09:35 +0800

    新增：接收枚举对象的构造器

--- 后端/平台系统后端代码/yygh_parent/common/service_utils/src/main/java/com/atguigu/yygh/common/exception/YyghException.java
+++ 后端/平台系统后端代码/yygh_parent/common/service_utils/src/main/java/com/atguigu/yygh/common/exception/YyghException.java
@@ -1,5 +1,6 @@
 package com.atguigu.yygh.common.exception;
 
+import com.atguigu.yygh.common.result.ResultCodeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,10 @@ public class YyghException extends RuntimeException {
     private Integer code;  //异常状态码
 
     private String msg;  //异常描述
+
+    // ⭐ 新增：直接接收枚举对象的构造器
+    public YyghException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage()); // 从枚举里取出信息交给父类
+        this.code = resultCodeEnum.getCode(); // 从枚举里取出状态码
+    }
 }

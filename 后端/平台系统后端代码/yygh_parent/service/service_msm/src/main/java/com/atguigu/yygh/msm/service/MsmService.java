0f4a15c8ce5a4a24e3626be3af6a94b134d4df83
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-08-03 18:24:44 +0800

    service:根据手机号发送短信验证码

--- 后端/平台系统后端代码/yygh_parent/service/service_msm/src/main/java/com/atguigu/yygh/msm/service/MsmService.java
+++ 后端/平台系统后端代码/yygh_parent/service/service_msm/src/main/java/com/atguigu/yygh/msm/service/MsmService.java
@@ -1,7 +1,11 @@
 package com.atguigu.yygh.msm.service;
 
+import com.atguigu.yygh.vo.msm.MsmVo;
+
 public interface MsmService {
 
     ////根据手机号发送短信验证码
-    boolean sendMsm(String phone, String code);
+    boolean sendMessage(String phone, String code);
+
+    boolean send(MsmVo msmVo);
 }

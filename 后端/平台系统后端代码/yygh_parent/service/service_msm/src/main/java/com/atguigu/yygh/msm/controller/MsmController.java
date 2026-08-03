328f685b490c9cae5c08f7b65eec8a81097a42f2
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-08-03 18:23:09 +0800

    controller:根据手机号发送短信验证码

--- 后端/平台系统后端代码/yygh_parent/service/service_msm/src/main/java/com/atguigu/yygh/msm/controller/MsmController.java
+++ 后端/平台系统后端代码/yygh_parent/service/service_msm/src/main/java/com/atguigu/yygh/msm/controller/MsmController.java
@@ -1,13 +1,16 @@
 package com.atguigu.yygh.msm.controller;
 
+import com.atguigu.yygh.common.exception.YyghException;
 import com.atguigu.yygh.common.result.R;
+import com.atguigu.yygh.common.result.ResultCodeEnum;
 import com.atguigu.yygh.msm.service.MsmService;
+import com.atguigu.yygh.msm.utils.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/msm")
@@ -20,24 +23,25 @@ public class MsmController {
     private RedisTemplate<String, String> redisTemplate;
 
     //根据手机号发送短信验证码
-    @GetMapping(value = "/send/{phone}")
+    @PostMapping(value = "/send/{phone}")
     public R code(@PathVariable String phone) {
 
-//        String code = redisTemplate.opsForValue().get(phone);
-//        if(!StringUtils.isEmpty(code)) return R.ok();
-//        //生成验证码
-//        code = RandomUtil.getFourBitRandom();
-//        //调用service方法发送
-//        boolean is_success = msmService.sendMsm(phone,code);
-//        //如果发送成功，把验证码放到redis，设置有效时长
-//        if(is_success) {
-//            //key：手机号  value：验证码
-//            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
-//            return R.ok();
-//        } else {
-//            return R.error();
-//        }
-        return R.ok();
-
+        String code = redisTemplate.opsForValue().get(phone);
+        if(StringUtils.hasText(code)) {//“是否包含真正的文本”
+            // 就不往下走了，直接返回成功，骗过前端，防止重复扣费
+            return R.ok().message("验证码尚未过期，请注意查收");
+        }
+        //生成验证码
+        code = RandomUtil.getFourBitRandom();
+        //调用service方法发送
+        boolean is_success = msmService.sendMessage(phone,code);
+        //如果发送成功，把验证码放到redis，设置有效时长
+        if(is_success) {
+            //key：手机号  value：验证码
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+            return R.ok();
+        } else {
+            return R.error().message("短信发送失败");
+        }
     }
 }
